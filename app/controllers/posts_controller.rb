20c9eb8660fa749edb7401fef1c334678d8b433a
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-09 21:26:32 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(post_params)
+
     redirect_to post
   end
 
@@ -24,3 +25,16 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
+
+# 
+# {"utf8"=>"✓",
+#  "authenticity_token"=>
+#   "y/ZgWgVmdGLnjMEEzP3W683cp26Nq/GAcD0vpRS4By9eIXd/7Lzjm9rYbUxwTu8mZfSDdYdJ4Bu1oLZS0CgXvA==",
+#  "post"=>
+#   {"title"=>"post2",
+#    "content"=>"content2",
+#    "category_ids"=>[""],
+#    "categories_attributes"=>{"0"=>{"name"=>"category2"}}},
+#  "commit"=>"Create Post",
+#  "controller"=>"posts",
+#  "action"=>"create"}
