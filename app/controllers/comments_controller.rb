8d65cf70324c30d994bcc2f213ec8f8c183e3424
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-11 08:29:10 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     binding.pry
+
     redirect_to comment.post
   end
 
@@ -12,3 +13,11 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
+
+# {"utf8"=>"✓",
+#  "authenticity_token"=>
+#   "A1l8fwU/6NjQGdpqnjuNfEleSg46N/1pi+0JIU33YzPq2o9LNGDZf63q1N/j0CsuultrFXco/LFhfnUTeenENw==",
+#  "comment"=>{"content"=>"comment5", "user"=>"1"},
+#  "commit"=>"Create Comment",
+#  "controller"=>"comments",
+#  "action"=>"create"}
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user       # Not sure about this!
+
+  accepts_nested_attributes_for :user, reject_if: :all_blank       # Not sure about this!
 
 end
+
+
+# accepts_nested_attributes_for :user, reject_if: :reject
+#
+#  def reject(attributes)
+#    attributes['username'].blank?
+#  end
