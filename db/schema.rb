5f0739f3bcf1fb517fcb67f8f30daf096cc5f1da
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-29 15:17:03 +0100

    seed database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119152016) do
+ActiveRecord::Schema.define(version: 20181229140841) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160119152016) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "comment_id"
   end
 
   create_table "users", force: :cascade do |t|
