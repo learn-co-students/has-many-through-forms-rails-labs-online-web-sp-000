debec58141f21aea06800b09d4a1c6f8d33a6f30
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-24 11:04:53 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,23 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_attributes].none? { |k,v| v == "" }
+      new_user = User.create(username: params[:comment][:user_attributes][:username], email: params[:comment][:user_attributes][:email])
+      comment.user = new_user
+      comment.save
+      redirect_to post_path(comment.post)
+    else
+      user = User.find(params[:comment][:user_id])
+      comment.user = user
+      comment.save
+      redirect_to post_path(comment.post)
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
+  # user_attributes:[:username, :email]
 end
