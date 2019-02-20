796528d927b9c5a21f55909fb161937297a56f46
github username <github email address>
2019-02-19 23:15:57 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  def user_attributes=(user_attributes)
+    if !user_attributes[:username].blank?
+      user = User.create(username: user_attributes.values.first)
+      user.comments << self
+    end
+  end
 
 end
