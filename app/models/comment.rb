07cd88850178dd0862faf538e7d2df92b1dc1dbd
Anup Khadka <akhadka.dev@gmail.com>
2019-04-05 19:59:55 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attribute)
+    if !user_attribute[:username].empty?
+      user = User.find_or_create_by(user_attribute)
+      self.user = user
+    end
+  end
+
 end
