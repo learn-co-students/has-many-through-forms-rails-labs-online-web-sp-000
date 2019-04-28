36923bda31a9b17b890db4928135323bd76b3afc
learn <36858744+decentralvision@users.noreply.github.com>
2019-04-27 20:59:50 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, :reject_if => :empty_fields
 
+  def empty_fields(attributed)
+    attributed[:username].blank?
+  end
 end
