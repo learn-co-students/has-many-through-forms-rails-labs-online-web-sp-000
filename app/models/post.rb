cca12f623a9a19a75fed8fbf47dc9596ba07f871
Btate712 <btate712@gmail.com>
2019-05-14 10:34:19 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,22 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
+  def unique_commentors
+    unique_user_list = []
+    self.comments.each do |comment|
+      if !unique_user_list.include?(comment.user)
+        unique_user_list << comment.user
+      end
+    end
+    unique_user_list
+  end
 end
