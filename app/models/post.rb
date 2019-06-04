ceab6db0df087e200eb63add1b65c21757cd30a1
Diana Badas <diana.badas@gmail.com>
2019-06-04 22:47:14 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories, reject_if: :all_blank
+  accepts_nested_attributes_for :comments, reject_if: :all_blank
 
   def comments_attributes=(comments_attributes)
     comments_attributes.values.each do |comment_attribute|
@@ -11,6 +12,18 @@ class Post < ActiveRecord::Base
       self.comments << comment
     end
   end
+
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(category_attributes)
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
+
 
 
 
