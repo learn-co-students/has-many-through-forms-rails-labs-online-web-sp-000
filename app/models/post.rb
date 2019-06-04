17d92e8463dcbde8b0f22bc8792a5c22efc47e0e
Diana Badas <diana.badas@gmail.com>
2019-06-04 16:30:57 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
+
+  def comments_attributes=(comments_attributes)
+    comments_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment
+    end
+  end
+
 
 
 end
