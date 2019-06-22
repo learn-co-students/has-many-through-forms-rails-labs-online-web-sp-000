a491c6b88d46a008e21d45819e8abecc812accfc
Tommy Curley <thomas.curley93@gmail.com>
2019-06-22 12:02:32 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+super_cute = Category.create(name: "Super Cute")
+really_funny = Category.create(name: "Really Funny")
+
+big_post = Post.create(title: "Big Post", content: "Lots of content")
+little_post = Post.create(title: "Little Post", content: "Not a lot of content")
+
+user1 = User.create(username: "Tom")
+user2 = User.create(username: "Joe")
+
+comment1 = Comment.create(content: "Really Cool!", user_id: 1, post_id: 1)
+comment2 = Comment.create(content: "Awful post", user_id: 1, post_id: 2)
+comment3 = Comment.create(content: "Great content!", user_id: 2, post_id: 1)
+comment4 = Comment.create(content: "I take back what I said earlier", user_id: 1, post_id: 2)
+
+big_post.categories << really_funny
+little_post.categories << really_funny
+little_post.categories << super_cute
+
