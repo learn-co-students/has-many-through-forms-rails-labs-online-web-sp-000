bc61677766451936edc080228881dc5f57ace727
ch2isk4kos <ch2isk4kos@gmail.com>
2019-06-25 20:23:10 -0400

    Done.

---
+++ NOTES.md
@@ -0,0 +1,18 @@
+@user   --<     @comments     >--       @post
+
+                                          |
+                                          ^
+
+                                    @post_category    
+
+                                          v
+                                          |
+
+                                      @category        
+
+
+
+@user --< @rankings >-- @sports
+                            |
+                            ^
+                        @players
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
 
-  def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
-  end
+    def new
+        @comment = Comment.new
+    end
 
-  private
+    def create
+        @comment = Comment.create(comment_params)
+        redirect_to @comment.post
+    end
 
-  def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
-  end
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    end
+
 end
