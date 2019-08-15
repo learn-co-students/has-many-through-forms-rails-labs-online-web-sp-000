5e5533c5e3e93a7fbe58accf72c0259df21e894a
Andrew Davisson <akdavisson4@gmail.com>
2019-08-15 14:27:17 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,11 +4,11 @@ class Comment < ActiveRecord::Base
 
   accepts_nested_attributes_for :user, reject_if: :user_reject?
 
-  def user_reject?(users_attributes)
-    users_attributes[:username].blank?
+  def user_reject?(attributes)
+    attributes[:username].blank?
   end
 
-  def user_attributes=(user)
+  def users_attributes=(user)
     self.user = User.find_or_create_by(username: user[:username])
     self.user.update(user)
   end
