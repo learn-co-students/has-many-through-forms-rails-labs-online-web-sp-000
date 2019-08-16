cc4b720cb0494908ace232e48ec5e914b152c534
Andrew Davisson <akdavisson4@gmail.com>
2019-08-16 11:14:25 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,8 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      #self.categories << category  #inefficient because it returns all categories
+      self.post_categories.build(category: category)
     end
   end
 
