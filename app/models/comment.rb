71451460de1cefa870de172502495ee498fa094a
Ramon Chavez <rachavez1974@gmail.com>
2019-08-21 17:34:21 -0400

    Finish Post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,15 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes_username=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.users << user
+    end
+  end
+
+  def users_attributes_username
+    self.user ? self.user.name : nil
+  end
+
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -45,7 +45,7 @@ describe 'Posts', type: 'feature' do
 
   describe 'show' do
     before do
-      @category = Category.create(name: "Cool")
+      @category =   Category.create(name: "Cool")
       @post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
       @post.categories << @category
       @user = User.create(username: 'RealBigFish')
