57ea9d0a8db6c6215c961202a09bd5e203550409
Indre Bakeer <indre.bakeer@gmail.com>
2019-09-02 02:26:50 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes=(attributes)
+    user_attributes.values.each do |attribute|
+      self.user = User.find_or_create_by(attribute)
+    end
+  end
+
 end
