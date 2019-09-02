1c93590e21379bad3f199bda7b6ab1f77423f2fb
Indre Bakeer <indre.bakeer@gmail.com>
2019-09-02 04:04:52 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def create
+    user = User.create(:user)
+  #  redirect_to comment.post
+  end
+
   def show
     @user = User.find(params[:id])
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
-  def users_attributes=(attributes)
-    user_attributes.values.each do |attribute|
-      self.user = User.find_or_create_by(attribute)
-    end
-  end
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
   def user_username
     self.user.username
