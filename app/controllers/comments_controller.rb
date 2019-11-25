fef6171ddcd49e9d712636f1f965e798dacce701
Lauren McCoy <lm_lutec@outlook.com>
2019-11-25 14:13:28 -0500

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,9 @@ class CommentsController < ApplicationController
     redirect_to comment.post
   end
 
+  def show
+    comment = Comment.find(params[:id])
+  end 
   private
 
   def comment_params
