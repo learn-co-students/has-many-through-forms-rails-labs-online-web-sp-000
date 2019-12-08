cf84d45aefdb2d172f254e5cbdd94c318222300c
Bernadette Davis <bsd231@nyu.edu>
2019-12-08 18:04:20 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,10 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(categories_attributes)
     categories_attributes.values.each do |categories_attribute|
-      category = Category.find_or_create_by(categories_attribute)
-      self.post_categories.build(category: category)
+      if categories_attribute[:name].present?
+        category = Category.find_or_create_by(categories_attribute)
+        self.post_categories.build(category: category)
+      end
     end
   end
 
