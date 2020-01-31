b249472ce2ec59a5ea24559c1d13f6c3a1206446
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-31 18:21:00 -0500

    Added all relevant code to finish lab. All test are passing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,16 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def users_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if user_attribute["username"].present?
+        user = user.find_or_create_by(user_attribute)
+      end
+    end
+  end
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
+
 end
