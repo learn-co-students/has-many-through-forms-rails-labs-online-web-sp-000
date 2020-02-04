7e2c214f071fd0b6526afc322a465888fe975f49
rohanphillips <rohan@solutions-insurance.com>
2020-02-03 20:08:51 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :users
+  accepts_nested_attributes_for :post_categories, reject_if: :all_blank
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
