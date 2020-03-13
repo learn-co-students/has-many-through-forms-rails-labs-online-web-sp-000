b3452faddca53d9a2121220064b29c54fd4157d5
github username <github email address>
2020-03-12 19:37:56 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,12 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
   def categories_attributes=(category_attributes)
+
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
+      if !category_attribute[:name].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category)
+      end
     end
   end
 
