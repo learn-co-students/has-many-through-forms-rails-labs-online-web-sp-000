294bc7ba686624560cdd2c33cedaeb837c07387d
RickLearnsToCode <rick.rosetti@gmail.com>
2020-03-29 14:20:52 -0600

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,27 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
     # binding.pry
+    if params[:comment_user_attributes_username] == ""
+      comment = Comment.create(comment_params)
+      # binding.pry
+      redirect_to comment.post
+    else
+    # binding.pry
+    comment = Comment.new
+    user = User.find_or_create_by(username: params[:comment_user_attributes_username])
+    comment.content = params[:comment][:content]
+    comment.post_id = params[:comment][:post_id]
+    comment.user_id = user.id
+    # binding.pry
+    comment.save
     redirect_to comment.post
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,9 +6,15 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(categories_attributes)
-    category_attributes.values.each do |a|
-      category = Category.find_or_create_by(a)
-      self.categories << category
+    categories_attributes.values.each do |a|
+      if a[:name] != ""
+        category = Category.find_or_create_by(a) unless a[:name] == ""
+      end
+      # binding.pry
+      if !!category
+        self.categories << category
+      end
+
     end
   end
 
