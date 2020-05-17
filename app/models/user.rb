9d21b6a65299fcf1e19f6939d64c0a07496ce440
ottocedeno <otto@creativespaceclub.com>
2020-05-17 19:34:17 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
 
-  def user_attributes=(user_hash)
-    self.user = User.find_or_create_by(user_hash)
-  end
+  # def user_attributes=(user_hash)
+  #   self.user = User.find_or_create_by(user_hash)
+  # end
 end
