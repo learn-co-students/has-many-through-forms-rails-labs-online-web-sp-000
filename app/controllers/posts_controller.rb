e6ec4aa8d78ab2c172ef6ec6d916030bce75010f
ottocedeno <otto@creativespaceclub.com>
2020-05-17 19:27:16 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.build_user
+    @users = User.all
   end
 
   def index
@@ -9,6 +12,8 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
+    @categories = Category.all
   end
 
   def create
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_hash)
+    unless category_hash.value?("")
+      self.categories << Category.find_or_create_by(category_hash)
+    end
+  end
 
+  def unique_users
+    self.users.uniq
+  end
 end
