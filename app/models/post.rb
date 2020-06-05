307df56d0d9c1c3833812b920647a535b20ee55b
dayofgray <graysonpberger@gmail.com>
2020-06-04 22:21:34 -0400

    Revert "Done."
    This reverts commit 9a27c151dc032177ad3a5f2fc5551f0259ebb16d.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,5 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  # {"title"=>"grayson's post 2", "content"=>"content of the post", "category_ids"=>["", "1"], "categories"=>{"name"=>"newish"}}
-  def categories_attributes=(categories_attributes)
-    categories_attributes.values.each do |category_attributes|
-      category = Category.find_or_create_by(category_attributes)
-      self.categories << category
-    end
-  end
 
 end
