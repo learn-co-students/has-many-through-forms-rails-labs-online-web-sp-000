70394f27dbd7bd8708d0a889e6e83e2f314a234b
Brian Vehige <brianvehige@gmail.com>
2020-06-05 11:34:53 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  #accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank?}
 
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attributes|
+      if user_attributes[:username].present?
+        user = User.find_or_create_by(user_attributes)
+        if !self.user = user
+          self.build_user(:username => user[:username])
+        end
+      end
+    end
+  end
+  
+  #ActionController::Parameters {"utf8"=>"✓", "authenticity_token"=>"M6WiwSLMqzmFrO890ceCQLVE26w6do/etB9v4VC0xGKUw6y9xAOWYuQRiih9sFj/4qCzMA67lzBJlOSYxODkNA==", "comment"=>{"content"=>"Test 3", "user_id"=>"", "user_attributes"=>{"username"=>"lukethedog"}, "post_id"=>"2"}, "commit"=>"Create Comment", "controller"=>"comments", "action"=>"create"} permitted: false>
 end
