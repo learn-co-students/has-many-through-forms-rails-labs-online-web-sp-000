34bc96d44499be987d28492c940207a1ddec2689
shaquille-galimba <shaqrivergalimba24@gmail.com>
2020-06-13 22:38:04 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@category = Category.create(name: "Cool")
+@post = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+@user = User.create(username: 'RealBigFish')
+
+
+@post.categories << @category
+@comment = Comment.create(content: "This was great!", user: @user, post: @post)
+
+@post1 = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+@user1 = User.create(username: 'CoolGuy5')
+@comment1 = @post1.comments.build(content: "Great Post!", user: @user1)
+@post1.save
+@comment2 = Comment.create(content: "And another thing, how come there aren't any vegetabls at the movie theater...", user: @user1, post: @post1)
