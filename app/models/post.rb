16fc6a5d01bb08353fe7b187b02d255cabd16fa9
CoderJay06 <jaycru06@yahoo.com>
2020-06-16 21:00:01 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :categories, through: :post_categories # all this does is add methods
   has_many :comments
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
