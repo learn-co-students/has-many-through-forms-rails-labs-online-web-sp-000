9acce837e1e11716781d80a9cf8406f71d0242c3
mlgvla <mlgerardvla@gmail.com>
2020-06-21 18:23:58 -0400

    comparing

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,11 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     binding.pry
     category_attributes.values.each do |category_attribute|
-      if category_attribute[:name].present?
+      if category_attribute["name"].present?
         category = Category.find_or_create_by(category_attribute)
         self.categories << category
       end
     end    
   end
 end
+
