88a6307542f64189908c8d777a61b2b2350334d0
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-15 21:37:45 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  
+  accepts_nested_attributes_for :user, reject_if: :reject_user
+
+  private
+
+  def reject_user(attributes)
+    attributes['username'].blank?
+  end
 
 end
