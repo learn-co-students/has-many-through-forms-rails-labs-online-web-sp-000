0ad186fa352b4595c30e616d9782ee4071207ad8
Joseph <DeGregorio.Joseph@gmail.com>
2020-07-20 13:49:31 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :reject_posts
+ 
+  def comments_attributes=(comment_attributes)
+     comment_attributes.values.each do |comment_attribute|
+       comment = Comment.find_or_create_by(comment_attribute)
+       self.comments << comment
+    end
+  end
 
-
+  def reject_posts(attributes)
+    attributes['name'].blank?
+  end
 end
