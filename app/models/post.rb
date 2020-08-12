c5eaaeecb72f933adbff3f75dfab67b674ea8a72
Mark A Guza <guzamaguza@gmail.com>
2020-08-12 05:16:39 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,13 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(attributes)
+    attributes.values.each do |attr|
+      if !attr[:name].blank?
+        category = Category.find_or_create_by(attr)
+        self.categories << category
+      end
+    end
+  end
 
 end
