54b8f00f8a12f43d4462b2409f4281023d658169
chienleow <chienleow1@gmail.com>
2020-09-30 23:43:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
-      if !category_attribute[:name].empty?
+      if category_attribute[:name].present?
         category = Category.find_or_create_by(category_attribute)
         self.categories << category
       end
