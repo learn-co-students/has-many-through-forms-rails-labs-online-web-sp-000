0965a0b184a9bdb725c477d9774c53742c2686e8
zacharytq <zacharytq@gmail.com>
2020-10-06 17:35:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,26 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories
 
+	def categories_attributes=(category_attributes)
+			category_attributes.values.each do |category_attribute|
+				category = Category.find_or_create_by(category_attribute)
+				if !category.name.empty?
+					self.categories << category
+				end
+			end
+  end
+
+  ##def categories_ids=(ids)
+	##	if !ids.empty?
+	##		ids.each {|id| self.categories << Category.find(id)}
+	##	end
+	##end
+
+	def one_user_comment
+		self.comments.map{|c| c.user}.uniq
+	end
+	
 
 end
