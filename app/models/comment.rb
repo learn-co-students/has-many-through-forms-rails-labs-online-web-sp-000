321a576f8521fc90185c9bb16ff51931d422b40b
blumenthaler <blumenthaleralex@gmail.com>
2020-10-25 19:03:09 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+  # silently ignore any new record hashes if they fail to pass criteria; super helpful
+  # Docs: https://api.rubyonrails.org/classes/ActiveRecord/NestedAttributes/ClassMethods.html
 
+  def username
+    self.user.username
+  end
 end
