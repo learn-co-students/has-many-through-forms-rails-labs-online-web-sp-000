a57ad361488645b532972d7b23f642bbc69e8468
SVRourke <samuel@rourke.tech>
2020-10-27 17:12:13 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: proc {|atts| atts['username'].blank? || User.find_by(username: atts['username']) }
 end
