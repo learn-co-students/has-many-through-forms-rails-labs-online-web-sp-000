65b35ba4a63d75c427a4392712cb79fe5595f26b
Mark May <mamark90@gmail.com>
2020-11-10 15:58:49 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+  	 category_attributes.values.each do |category_attribute|
+  	 	 category = Category.find_by(name: category_attribute[:name])
+
+       if !category.nil?
+         self.categories << category
+       else
+        self.categories << Category.create(name: category_attribute[:name]) unless category_attribute[:name].empty?
+  	   end
+     end
+
+  end
 
 end
