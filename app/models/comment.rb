9389b2ab8e2fc78bb0f779cf859b98b94cbe7ee9
seth <seth.j.near@gmail.com>
2020-11-17 21:07:11 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(user)
+    unless (user[:username] == "") || (user[:username] == nil)
+    self.user = User.find_or_create_by(username: user[:username])
+    self.save
+    end
+  end
+
 end
