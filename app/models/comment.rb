e6c0d1c9066e2e6ee8eccae4cc21a9169fe70e55
Chad Lengefeld <chadlengefeld@gmail.com>
2020-12-02 06:15:40 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,10 @@ class Comment < ActiveRecord::Base
     self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
   end
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
+  end
+
 end
