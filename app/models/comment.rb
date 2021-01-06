93844f0615886001e46dc4893effe43c44c76566
kkirby16 <kkirby16@my.whitworth.edu>
2021-01-05 20:01:38 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :reject_user
+
+  def reject_user(attributes)
+    attributes.each do |key, value|
+      value.blank?
+    end
+  end
 end
