bcfe3468f2d85182b7756517f919bfe4f16f5e34
kkirby16 <kkirby16@my.whitworth.edu>
2021-01-07 11:28:26 -0800

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,9 @@ class CommentsController < ApplicationController
     redirect_to comment.post
   end
 
-  def user=(user)
-    self.user = User.find_or_create_by(name: user[:username])
-    self.user.update(user)
-  end
-
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, users_attributes: [:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
+require "pry"
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: :reject_user
+  accepts_nested_attributes_for :user
 
-  def reject_user(attributes)
-    attributes.each do |key, value|
-      value.blank?
-    end
+  def user_attributes=(user_attributes)
+    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank?
   end
 end
