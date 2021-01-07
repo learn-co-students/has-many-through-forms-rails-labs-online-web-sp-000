f41245d8674388795718e1068875980d804b82ca
kkirby16 <kkirby16@my.whitworth.edu>
2021-01-06 20:06:12 -0800

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
+require "pry"
+
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
 
+  def user=(user)
+    self.user = User.find_or_create_by(name: user[:username])
+    self.user.update(user)
+  end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes: [:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, users_attributes: [:username])
   end
 end
