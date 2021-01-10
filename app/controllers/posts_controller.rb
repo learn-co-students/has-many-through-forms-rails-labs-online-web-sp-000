ac584d0179f59a05d53c9873c4c9444ba9547556
john million <jpmillion@comcast.net>
2021-01-10 16:05:54 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment: [:content, :post_id, user_attibutes: [:username]])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,24 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
-  has_many :users, through: :comments
+  has_many :users, -> { distinct }, through: :comments
 
+  def categories_attributes=(category_hashes)
+    category_hashes.each do |i, category_attributes|
+      if category_attributes[:name].present?
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        #self.categories << category
+        if !self.categories.include?(category)
+          self.post_categories.build(category: category)
+        end
+      end
+    end
+  end
 
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |attribute|
+      self.comments.build(comment)
+    end
+  end
+  
 end
