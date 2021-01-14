490a6c2f395eb59ea7255667dfbc9a7e1f0d827f
Catherine Gloria <catherinegloria@gmail.com>
2021-01-14 13:38:15 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(categories_hashes)
-    categories_hashes.each do |index, category_attribute|
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each do |index, category_attribute|
       if category_attribute[:name].present?
         category = Category.find_or_create_by(name: category_attribute[:name])
         self.post_categories.build(:category => category) unless self.categories.include?(category)
