1e701dd41169419a649519a8212fc655b2852009
joseph <josephtkao@gmail.com>
2021-02-08 14:32:52 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,19 @@
+require 'pry'
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
 
-  accepts_nested_attributes_for :categories
-
+  accepts_nested_attributes_for :categories, reject_if: proc {|category| category[:name].blank?}
   
+  # def category_attributes=(category_attributes)
+  #   category_attributes.values.each do |category|
+  #     if category[:name].present?
+  #       self.categories << Category.find_or_create_by(category)
+  #     end
+  #   end
+  # end
+
 
 end
