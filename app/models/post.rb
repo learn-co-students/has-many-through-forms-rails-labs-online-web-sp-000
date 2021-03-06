90c99327b0892a10bf1d19c473788b07baa77a99
RSigmaSG <sg.comparch@gmail.com>
2021-03-06 01:55:32 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,27 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments 
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if !category_attribute["name"].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment|
+      if !category_attribute["name"].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
+    end
+  end
 
+  def unique_commenting_users
+    self.comments.collect{|comment| comment.user}.uniq
+  end
 end
