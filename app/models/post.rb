74483bf8f983e7e8671e4e9087c5642639e72dd4
Jose Melero Martinez <jmelero08@gmail.com>
2021-04-06 14:40:29 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,18 @@
+
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if !category_attribute[:name].empty?
+        category = Category.find_or_create_by(category_attribute)
+        self.post_categories.build(category: category)
+        self
+      end
+    end
+end
 
 end
