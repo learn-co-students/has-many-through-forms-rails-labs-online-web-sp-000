8a27a0fde55e83ba6d1cad8e4b8d704f25ecbf90
dbrandt1990 <dvbrandt90@gmail.com>
2021-04-12 16:16:29 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,15 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :blank_category
+  accepts_nested_attributes_for :comments, reject_if: :blank_comment
+
+  def blank_category(attributes)
+    attributes[:name].blank?
+  end
+
+  def blank_comment(attributes)
+    attributes[:content].blank?
+  end
 
 end
