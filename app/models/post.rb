fcb94c6d20b96384527642453c36fd8fccbc7761
Allison <allimacq@gmail.com>
2021-04-26 14:07:58 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories)
+    categories.values.each do |c|
+      category = Category.find_or_create_by(c)
+      if category.name.empty? == false
+        self.categories << category
+      end
+    end
+  end
+
 
 end
