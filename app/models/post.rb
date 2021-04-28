6ffee7e3c9dee36dbd0f11dcfeb40e995e5b7c13
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-04-28 13:39:17 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,16 +4,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :comments, reject_if: :reject_comments
+  accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
     end
-  end
-
-  def reject_comments(attributes)
-    attributes['content'].blank?
   end
 end
