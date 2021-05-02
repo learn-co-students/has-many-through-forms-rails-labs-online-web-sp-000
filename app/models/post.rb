8d3de010b7454160179c4a42144b3b40e13a28b4
Jess Pesale <jess_pesale@aol.com>
2021-05-02 17:08:29 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories
 
+
+  # def categories_attributes=(attributes)
+  #   attributes.values.each do |v|
+  #     self.categories << categories.find_or_create_by(v) if !v['name'].empty?
+  #   end
+  # end
 
 end
