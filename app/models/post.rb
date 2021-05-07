6968e022aa0f20f54a4b024e88feb2b632ec14e6
Damian Malysza <damian.malysza@gmail.com>
2021-05-06 20:55:50 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(attributes)
+    attributes.values.each do |attribute|
+      unless attribute["name"].empty?
+        category = Category.find_or_create_by(attribute)
+        self.categories << category
+      end
+    end
+  end
+
+  def unique_commenters
+    commenters = []
+    self.comments.each do |comment|
+      commenters << comment.user
+    end
+    commenters.uniq
+  end
 
 end
