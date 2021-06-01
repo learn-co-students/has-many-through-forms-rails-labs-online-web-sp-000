b01d7f50514f9d59563a19ec81825611380aa258
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-06-01 15:51:43 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,20 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories 
 
+  def categories_attributes=(cats_atts)
+    cats_atts.values.each do |cat_att|
+      if !cat_att[:name].empty?
+        category = Category.find_or_create_by(cat_att)
+        self.post_categories.build(category: category)
+      end
+    end
+  end
+
+  def unique_users
+    self.users.uniq
+  end
+  
 
 end
