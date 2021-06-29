20e0ea99231445843d5b7c6267b1ce0eecda5854
William Pannell <lpann5@gmail.com>
2021-06-28 21:33:57 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :all_blank
+  accepts_nested_attributes_for :user
 
   def user_attributes=(user_attributes)
-      self.user = User.find_or_create_by(username: user_attributes[:username]) 
+    if !(user_attributes[:username].blank?)
+    self.user = User.find_or_create_by(username: user_attributes[:username])
+    end
   end
+
 end
