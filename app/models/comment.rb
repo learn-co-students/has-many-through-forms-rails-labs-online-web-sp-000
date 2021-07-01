aa1e34ed3be2e6ce6af7610ae19c54557ee01208
Raven <ravenkoehler@gmail.com>
2021-07-01 11:01:32 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,4 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  def comment_user_id=(id)
-    @user = User.find(id)
-    self.user = @user
-  end
-
-  def comment_user_id
-    self.user ? self.user.id : nil
-  end
-
 end
