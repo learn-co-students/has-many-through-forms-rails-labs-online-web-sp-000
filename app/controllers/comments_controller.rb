eb1718e14937a16975e1505fac4afa10fb59df33
Raven <ravenkoehler@gmail.com>
2021-07-01 20:27:02 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
+    byebug
     comment = Comment.create(comment_params)
+    if !params[:comment][:user_id]
+      @new_user = User.create(params[:user_attributes])
+      comment.user = @new_user
+    end
     redirect_to comment.post
   end
 
